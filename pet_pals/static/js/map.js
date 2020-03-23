e1371184cd3c7d9868789b757db1f9f00de50848
Zachary Morrow <morrowzl@vcu.edu>
2020-03-23 13:00:35 -0400

    added non breaking updates

---
+++ pet_pals/static/js/map.js
@@ -0,0 +1,22 @@
+var API_KEY = fetch('/mapboxkey', {credentials:'omit'}).then((response) => {
+  return response.json()}).then((key) => createMap(key));
+
+function createMap(key) {
+
+// Create a map object
+  var myMap = L.map("mappy-boi", {
+    //38.702969, -76.828331
+    center: [38.7029, -76.8283],
+    zoom: 10
+  });
+
+  var tile = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets-basic",
+    accessToken: key
+  });
+
+
+  tile.addTo(myMap);
+}
